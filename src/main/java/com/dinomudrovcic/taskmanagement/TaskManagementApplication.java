06fd6a05bf5fff57079075fda28fd61740a1a10a
dinomudrovcic <dinomudrovcic@gmail.com>
2022-02-19 13:33:43 +0100

    remove unused imports

--- src/main/java/com/dinomudrovcic/taskmanagement/TaskManagementApplication.java
+++ src/main/java/com/dinomudrovcic/taskmanagement/TaskManagementApplication.java
@@ -1,14 +1,8 @@
 package com.dinomudrovcic.taskmanagement;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import java.util.Arrays;
-import java.util.stream.Collectors;
 
 @SpringBootApplication
 @EnableJpaRepositories
